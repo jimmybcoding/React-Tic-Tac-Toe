a0e6baf02bcb06434cc4751282767d1ca92c25f5
James Barbour <jameskbarbour1@gmail.com>
2023-05-03 09:08:00 -0400

    Updates

---
+++ precache-manifest.46c9e148b6c95451d570210ed4b69948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85ca161d35763be5b3b5",
+    "url": "/React-Tic-Tac-Toe/static/css/main.222f7408.chunk.css"
+  },
+  {
+    "revision": "85ca161d35763be5b3b5",
+    "url": "/React-Tic-Tac-Toe/static/js/main.b1390063.chunk.js"
+  },
+  {
+    "revision": "02806f2f762087ad7298",
+    "url": "/React-Tic-Tac-Toe/static/js/runtime~main.0bda7406.js"
+  },
+  {
+    "revision": "4f8549dffa947a35abb9",
+    "url": "/React-Tic-Tac-Toe/static/js/2.b1c4f61e.chunk.js"
+  },
+  {
+    "revision": "cac8c4143a7a069d6b41b93ed9d3fce0",
+    "url": "/React-Tic-Tac-Toe/index.html"
+  }
+];
